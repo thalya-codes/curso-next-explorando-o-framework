b34960e96e6bcb0f0cdcffc130d524a9acd774f4
Thalya Stéffany <thalyacoiimbra@gmail.com>
2023-06-07 00:39:57 -0300

    feat(Footer): cria estrutura do componente

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="bg-zinc-800 py-10 text-center text-gray-400">
+            <p>© 2023 Thalya Stéffany. Todos os direitos reservados.</p>
+        </footer>
+    )
+}
