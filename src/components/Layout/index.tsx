aecdc7d332cc7cd2183895227fc26af47020e549
Thalya Stéffany <thalyacoiimbra@gmail.com>
2023-06-07 23:30:44 -0300

    feat(Layout): cria estrutura do componente

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,12 @@
+import { PropsWithChildren } from "react";
+
+export default function Layout({ children }: PropsWithChildren) {
+    return (
+        <main className="
+            flex flex-col gap-14 xl:flex-row xl:gap-0 
+            px-16 py-14 xl:py-24 min-h-screen w-screen md:mb-20"
+        >
+            { children }
+        </main>
+    )
+}
