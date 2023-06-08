ab834a9c15689d9e7f7df19ca908eb8e0512ddc9
Thalya Stéffany <thalyacoiimbra@gmail.com>
2023-06-07 21:50:06 -0300

    feat(FormGroup): implementa responsividade do componente

--- src/components/FormGroup/index.tsx
+++ src/components/FormGroup/index.tsx
@@ -1,12 +1,23 @@
 export default function FormGroup() {
     return (
-        <form className="flex gap-5">
+        <form className="flex flex-wrap sm:flex-nowrap justify-between gap-5 xl:justify-normal w-full">
             <input 
                 type="email" 
                 placeholder="Coloque seu email aqui"
-                className="rounded border-2 border-zinc-500 outline-none focus:border-indigo-800 shadow-md px-8 py-2"
+                className="
+                    w-full sm:w-[70%] xl:w-auto rounded 
+                    border-2 border-zinc-500 outline-none 
+                    focus:border-indigo-800 shadow-md px-8 py-2
+                "
             />
-            <button className="bg-indigo-700 hover:bg-indigo-500 py-2 px-10 rounded text-white">Cadastrar</button>
+            <button 
+                className="
+                    bg-indigo-700 hover:bg-indigo-500 
+                    py-2 px-10 rounded text-white
+                    w-full
+                    sm:w-auto
+                "
+            >Cadastrar</button>
         </form>
     )
 }
