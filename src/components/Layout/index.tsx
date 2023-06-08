9f875532ab45c3ea6130132fd8bcb68b3cfc1c7c
Thalya Stéffany <thalyacoiimbra@gmail.com>
2023-06-08 16:12:10 -0300

    fix(Layout): realiza ajuste no espaçamento do componente

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,9 +2,10 @@ import { PropsWithChildren } from "react";
 
 export default function Layout({ children }: PropsWithChildren) {
     return (
-        <main className="
+        <main className={`
             flex flex-col gap-14 xl:flex-row xl:gap-0 
-            px-16 py-14 xl:py-24 min-h-screen w-screen md:mb-20"
+            pt-14 xl:py-24 min-h-screen w-screen md:mb-20
+        `}
         >
             { children }
         </main>
