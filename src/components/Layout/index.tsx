be538a03fe9477c1b815e3f496d7709b7c55749d
Thalya Stéffany <thalyacoiimbra@gmail.com>
2023-06-08 16:13:49 -0300

    feat(Layout): adiciona nova prop ao componente

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,12 @@
 import { PropsWithChildren } from "react";
+import { ILayoutProp } from "@/interfaces/ILayoutProp";
 
-export default function Layout({ children }: PropsWithChildren) {
+export default function Layout({ children, classNames = "" }: PropsWithChildren<ILayoutProp>) {
     return (
         <main className={`
             flex flex-col gap-14 xl:flex-row xl:gap-0 
             pt-14 xl:py-24 min-h-screen w-screen md:mb-20
+            ${classNames}
         `}
         >
             { children }
---
+++ src/interfaces/ILayoutProp.ts
@@ -0,0 +1,3 @@
+export interface ILayoutProp {
+    classNames?: string;
+}
